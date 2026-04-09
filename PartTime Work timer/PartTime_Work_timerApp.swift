7708ba807f43a96ee27b56f5886c9771cf334676
Dmytro Mayevsky <mayevskydv@gmail.com>
2026-04-09 11:59:45 +0200

    Initial Commit

---
+++ PartTime Work timer/PartTime_Work_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartTime_Work_timerApp.swift
+//  PartTime Work timer
+//
+//  Created by Dmytro Mayevsky on 09.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct PartTime_Work_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
